502facbc1cdb2f2dbd28a7a75ab89d4c2c6c8973
Kayla <6lackmacaroon@gmail.com>
2019-06-07 13:41:35 -0600

    cities working after refactor/removal of helpers

--- api/routers/citiesRouter.js
+++ api/routers/citiesRouter.js
@@ -1,39 +1,50 @@
 const router = require('express').Router();
-
-const db = require('../../data/helpers/citiesDB');
-
-router
-      .get('/', (req, res) => {
-            db.getCities()
-                  .then(cities => {
-                        res.status(200).json(cities);
-                  })
-                  .catch(err => {
-                        res.status(500).json({ error: 'could not get anything from the database' })
-                  })
-      })
-      .post('/', (req, res) => {
-            db.addCity(req.body)
-                  .then(city => {
-                        res.status(201).json({ message: 'new city added'})
-                  })
-                  .catch(err => {
-                        console.log(err);
-                        res.status(500).json({ message: 'could not add to database' })
-                  })
-      })
+const db = require('../../data/configure.js');
+// const db = require('../../data/helpers/citiesDB');
 
 router
-      .get('/:id', (req, res) => {
-            const id = req.params.id;
-            db.getCity(id)
-                  .then(city => {
-                  res.status(200).json(city);
-                  })
-                  .catch(err => {
-                        console.log(err);
-                        res.status(500).json({ message: "Couldn't get that city.." });
-                  });
+      .get('/', async (req, res) => {
+            try {
+                  const cities = await db('cities')
+                  res.status(200).json(cities)
+            } catch (error) {
+                  console.log(error);
+                  res.status(500).json({ error: 'could not get anything from the database' })
+            }
+      })
+      .post("/", async (req, res) => {
+            try {
+                  const [id] = await db("cities").insert(req.body)
+                        .where({ id })
+                        .first();
+                  res.status(201).json({ message: "City added" });
+            } catch (error) {
+                  console.log(error);
+                  res.status(500).json({ error: "Could not add city" });
+            }
+          });
+
+router
+      .get('/:id', async (req, res) => {
+            try {
+                  const city = await db('cities as c')
+                        .select(
+                              'c.city_name',
+                              'c.city_description',
+                              'c.city_visited'
+                        )
+                        .where('c.id', req.params.id)
+                        .first()
+                  if(city) {
+                        res.status(200).json(city)
+                  } else {
+                        console.log(error)
+                        res.status(404).json({ error: 'that ID does not exist' })
+                  }
+            } catch (error) {
+                  console.log(error)
+                  res.status(500).json({ message: "Couldn't get that city.." });
+            }      
       })
 
 module.exports = router;
--- api/routers/countriesRouter.js
+++ api/routers/countriesRouter.js
@@ -3,37 +3,53 @@ const router = require('express').Router();
 const db = require('../../data/helpers/countriesDB');
 
 router
-      .get('/', (req, res) => {
-            db.getCountries()
-                  .then(countries => {
-                        res.status(200).json(countries);
-                  })
-                  .catch(err => {
-                        res.status(500).json({ error: 'could not get anything from the database' })
-                  })
+      .get('/', async (req, res) => {
+            try {
+                  const countries = await db('countries')
+                  res.status(200).json(countries)
+            } catch (error) {
+                  console.log(error);
+                  res.status(500).json({ error: 'could not get anything from the database' })
+            }
       })
-      .post('/', (req, res) => {
-            db.addCountry(req.body)
-                  .then(country => {
-                        res.status(201).json({ message: 'new country added'})
-                  })
-                  .catch(err => {
-                        console.log(err);
-                        res.status(500).json({ message: 'could not add to database' })
-                  })
-      })
+      .post("/", async (req, res) => {
+            try {
+                  const [id] = await db("countries").insert(req.body)
+                        .where({ id })
+                        .first();
+                  res.status(201).json({ message: "Country added" });
+            } catch (error) {
+                  console.log(error);
+                  res.status(500).json({ error: "Could not add country" });
+            }
+      });
 
 router
-      .get('/:id', (req, res) => {
-            const id = req.params.id;
-            db.getCountry(id)
-                  .then(country => {
-                  res.status(200).json(country);
-                  })
-                  .catch(err => {
-                        console.log(err);
-                        res.status(500).json({ message: "Couldn't get that country.." });
-                  });
+      .get('/:id', async (req, res) => {
+            try {
+                  const country = await db('countries as c')
+                        .select(
+                              'c.counrty_id',
+                              'c.country_name',
+                              'c.country_description',
+                              'c.country_visited'
+                        )
+                        .where('c.id', req.params.id)
+                        .first()
+                  const city = await db('cities as cit')
+                        .join('countries as c', 'c.id', 'cit.city_id')
+
+                              )
+                  if(country) {
+                        res.status(200).json(country)
+                  } else {
+                        console.log(error)
+                        res.status(404).json({ error: 'that ID does not exist' })
+                  }
+            } catch (error) {
+                  console.log(error)
+                  res.status(500).json({ message: "Couldn't get that country.." });
+            }      
       })
 
 module.exports = router;
